ad81ea94d7661a87745d591c16d10171b0ab9d5c
b0nsu <125778250+b0nsu@users.noreply.github.com>
2026-04-08 22:21:47 +0900

    build: migrate project to Gradle

--- build.bat
+++ build.bat
@@ -3,17 +3,9 @@ setlocal
 
 set ROOT_DIR=%~dp0
 set OUT_DIR=%ROOT_DIR%out
-set CLASS_DIR=%OUT_DIR%\classes
 set JAR_PATH=%OUT_DIR%\study-room-cli.jar
 
-if not exist "%OUT_DIR%" mkdir "%OUT_DIR%"
-if not exist "%CLASS_DIR%" mkdir "%CLASS_DIR%"
-if exist "%JAR_PATH%" del "%JAR_PATH%"
-
-javac -encoding UTF-8 -d "%CLASS_DIR%" "%ROOT_DIR%src\*.java"
-if errorlevel 1 exit /b 1
-
-jar --create --file "%JAR_PATH%" --main-class Main -C "%CLASS_DIR%" .
-if errorlevel 1 exit /b 1
+call "%ROOT_DIR%gradlew.bat" clean jar
+if errorlevel 1 exit /b %errorlevel%
 
 echo Build success: %JAR_PATH%
